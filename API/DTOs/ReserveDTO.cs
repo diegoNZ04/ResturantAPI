f1378cff3c8d21fc2b2837d941ad3259b02747f2
diegoNZ04 <diegoamorim03152004@gmail.com>
2025-04-02 16:59:41 -0300

    Deleções a partir da segregação

--- API/DTOs/ReserveDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace RestaurantAPI.DTOs
-{
-    public class ReserveDTO
-    {
-        public int TableId { get; set; }
-        public int UserId { get; set; }
-        public int PeopleNumber { get; set; }
-    }
-}
