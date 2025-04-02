0d19393f2325f1d3b5a2f588a01b73a81d742472
diegoNZ04 <diegoamorim03152004@gmail.com>
2025-04-02 17:11:07 -0300

    Retirando Data Annotations

--- Domain/Entities/Table.cs
+++ Domain/Entities/Table.cs
@@ -5,21 +5,9 @@ namespace RestaurantAPI.Domain.Entities
 {
     public class Table
     {
-        // Id
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        // Name
-        [Required(ErrorMessage = "O nome ou número da mesa é obrigatório.")]
-        [StringLength(100, ErrorMessage =
-            "O nome deve ter no máximo 100 caracteres.")]
         public string Name { get; set; }
-        // Capacity
-        [Required(ErrorMessage = "A capacidade da mesa é obrigatória")]
-        [Range(1, 12)]
         public int Capacity { get; set; }
-        // Status
-        [Required(ErrorMessage = "O status da mesa é obrigatório")]
         public TableStatus Status { get; set; }
     }
 }
